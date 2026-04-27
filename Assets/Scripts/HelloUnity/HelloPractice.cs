15b935413664f035068ba7ea4ec2af0df6a2d35d
ee2ssam <ee2ssam@gmail.com>
2026-04-27 16:16:05 +0900

    1. Hello World 테스트

---
+++ Assets/Scripts/HelloUnity/HelloPractice.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class HelloPractice : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+/*
+[Q]
+콘솔창에 아래 내용을 출력하세요
+Hello, Game
+123456789
+*/
