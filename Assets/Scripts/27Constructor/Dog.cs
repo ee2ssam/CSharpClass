c5fc2347957c98222044e84b76f55c9e9aa91bfb
ee2ssam <ee2ssam@gmail.com>
2026-05-15 15:20:43 +0900

    1. 생성자, 소멸자 2. 메서드(값형, 참조형, 반환형 전달 방법)

---
+++ Assets/Scripts/27Constructor/Dog.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Constructor
+{
+    //강아지를 관리하는 클래스
+    public class Dog
+    {
+        //[1]필드
+        private string name;    //이름
+
+        //[2]생성자 - 필드 초기값 설정
+        //매개변수로 문자열을 받아서 name에 대입
+        //생성자 호출시 강아지 이름 만들어 준다
+        public Dog(string str)
+        {
+            name = str;
+        }
+
+        //[3]메서드 - 강아지 이름을 반환하는 함수
+        public string GetName()
+        {
+            return name;
+        }
+
+    }
+}
